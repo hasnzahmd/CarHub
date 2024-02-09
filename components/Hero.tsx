1cf31607f8197ab48f7d6631031bdd428a0d4574
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-09 22:51:26 +0500

    update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <div className='hero'>
         <div className="flex-1 pt-36 padding-x">
             <h1 className="hero__title">
-                Find, book, or rent a car - quickly and easily!
+                Find, book, or rent a car -- quickly and easily!
             </h1>
             <p className="hero__subtitle">
                 Streamline your car rental experience with our effortless booking process.
@@ -25,8 +25,8 @@ const Hero = () => {
         <div className="hero__image-container">
             <div className="hero__image">
                 <Image src='/hero.png' alt='hero' fill className="object-contain"/>
-                <div className='hero__image-overlay'/>
             </div>
+            <div className='hero__image-overlay'/>
         </div>
     </div>
   )
