bba7b9e54ba8d2dc94ad983e3d1a6b0febfb1a30
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-13 23:31:54 +0500

    add car card interface

--- types/index.ts
+++ types/index.ts
@@ -12,3 +12,17 @@ export interface SearchManufacturerProps {
     manufacturer: string,
     setManufacturer: (manufacturer: string) => void;
 }
+
+export interface CarProps {
+    class: string,
+    city_mpg: number,
+    combination_mpg: number,
+    cylinders: number,
+    displacement: number,
+    drive: string,
+    fuel_type: string,
+    highway_mpg: number,
+    make: string, 
+    model: string,
+    transmission: string
+}
