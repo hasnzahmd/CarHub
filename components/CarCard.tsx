4d8c2132bca7f254e820b3b89d66cf5ab26a880b
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-13 23:32:48 +0500

    add carCard component

---
+++ components/CarCard.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { CarProps } from '@/types'
+import Image from 'next/image'
+import { useState } from 'react'
+import CustomButton from './CustomButton'
+
+interface CarCardProps {
+    car: CarProps
+}
+
+const CarCard = ({car}: CarCardProps) => {
+  return (
+    <div>CarCard</div>
+  )
+}
+
+export default CarCard
