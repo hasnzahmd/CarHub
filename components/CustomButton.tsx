12fc5040336df353ebc6064f3f4ef572b2737d23
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-14 01:45:44 +0500

    update

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
 
 const CustomButton = (props:CustomButtonProps) => {
   return (
@@ -9,9 +10,19 @@ const CustomButton = (props:CustomButtonProps) => {
         className={`custom-btn ${props.containerStyles}`}
         onClick={props.handleClick}
     >
-    <span className={`flex-1`}>
+    <span className={`flex-1 ${props.textStyles}`}>
         {props.title}
     </span>
+      {props.rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={props.rightIcon}
+            alt="right icon"
+            fill
+            className='object-contain'
+          />
+        </div>
+      )}
     </button>
   )
 }
