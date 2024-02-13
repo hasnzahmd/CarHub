7add192e61019a528c2c4f64bf93036a47b41f1c
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-14 01:46:48 +0500

    update

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,21 @@
 import Hero from "./Hero";
-import CustomBUtton from "./CustomButton";
+import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 export {
     Hero,
-    CustomBUtton,
+    CustomButton,
     Navbar,
     Footer,
     SearchBar,
     CustomFilter,
     SearchManufacturer,
-    CarCard
+    CarCard,
+    CarDetails
 }
